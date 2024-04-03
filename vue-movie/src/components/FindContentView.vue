0b8b45666dc4d00b66ea6b896ede0867bfe38b4a
ojiwon99 <dhwldnjs0420@naver.com>
2024-04-03 21:40:48 +0900

    2024-04-03

--- vue-movie/src/components/FindContentView.vue
+++ vue-movie/src/components/FindContentView.vue
@@ -1,44 +1,37 @@
 <template>
   <div class="modal" v-if="isOpen">
     <div class="modal-content">
-      <!-- 모달 내용 -->
-      <div class="ticket">
-        <div class="order-number">코난 영화관</div>
-        
-        <div class="info">
-          <strong>{{ m_name }}</strong>
-        </div>
-        
-        <div class="infoList">
-          {{ m_r_date }}
-        </div>
-
-        <div class="infoList">
-          <!-- 상영관 정보를 받아와야 함 -->
-          <!-- <span>상영관 : {{ theater }}</span><br><br> -->
-          <span>좌석번호 : {{ m_s_no }} (1명)</span>
-        </div>
-       
-        <div class="info">
-          [결제 내역]
-        </div>
-        <div class="infoList">
-          <span>영화가격 : {{ m_price }}원</span>
-        </div>
-        <div class="point">
-          <span>{{ user_name }} 님!</span><br>
-
-          <!-- 사용 가능 포인트 정보를 받아와야 함 -->
-        <span>사용 가능 포인트: {{ user_point }}점</span>
-        </div>
-
-        <!-- 바코드 이미지 추가 -->
-        <!-- 바코드 이미지 정보를 받아와야 함 -->
-        <img src="../assets/qrimages.png" alt="바코드" class="barcode">
+      <div class="modal-header">
+        <span class="close" @click="closeModal">&times;</span>
       </div>
-      
-      <!-- 닫기 버튼 -->
-      <span class="close" @click="closeModal">&times;</span>
+      <div class="modal-body">
+        <div class="ticket">
+          <div class="order-number">코난 영화관</div>
+          <div class="info">
+            <strong>{{ selectedMovie.m_name }}</strong>
+          </div>
+          <div class="infoList">
+            {{ selectedMovie.m_r_date }}
+          </div>
+          <div class="infoList">
+            <!-- 상영관 정보를 받아와야 함 -->
+            <span>좌석번호 : {{ selectedMovie.m_s_no }} (1명)</span>
+          </div>
+          <div class="info">
+            [결제 내역]
+          </div>
+          <div class="infoList">
+            <span>영화가격 : {{ selectedMovie.m_price }}원</span>
+          </div>
+          <div class="point">
+            <span>{{ selectedMovie.user_name }} 님!</span><br>
+            <!-- 사용 가능 포인트 정보를 받아와야 함 -->
+            <span>사용 가능 포인트: {{ selectedMovie.user_point }}점</span>
+          </div>
+          <!-- 바코드 이미지 추가 -->
+          <img :src="selectedMovie.barcodeImage" alt="바코드" class="barcode">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -50,25 +43,21 @@ export default {
       type: Boolean,
       required: true
     },
-    m_r_no: Number,
-    m_name: String,
-    s_date: String,
-    m_r_date: String,
-    m_s_no: String,
-    m_price: Number,
-    user_name: String,
-    barcodeImage: Image,
-    user_point: Number // 'int'를 'Number'로 수정
+    selectedMovie: {
+      type: Object,
+      default: () => ({})
+    }
   },
   methods: {
     closeModal() {
-      // 모달을 닫는 메서드
       this.$emit('close-modal');
+    }
+  },
+  computed: {
+    barcodeImage() {
+      // 선택된 영화의 바코드 이미지 URL 반환
+      return this.selectedMovie.barcodeImage || ''; // 바코드 이미지 URL이 없으면 빈 문자열 반환
     }
   }
 };
 </script>
-
-<style scoped>
-/* 필요한 CSS 스타일을 추가하세요 */
-</style>
--- vue-movie/src/views/find/FindView.vue
+++ vue-movie/src/views/find/FindView.vue
@@ -1,140 +1,151 @@
 <template>
     <div>
-      <div id="wrap">
-        <AppHeader />
-        <div id="app">
-          <div class="content" v-if="isModalViewed">
-            <!-- 예매 내역 조회 창 -->
-            <form action="" method="" enctype="multipart/form-data">
-              <div class="m-body">
-                <div id="result">
-                  <table id="result-table">
-                    <thead>
-                      <tr>
-                        <th>예약번호</th>
-                        <th>영화 제목</th>
-                        <th>상영일자</th>
-                        <th>상영시간</th>
-                        <th>상영관</th>
-                        <th>좌석번호</th>
-                        <th>영화가격</th>
-                        <th>구매자이름</th>
-                        <th>포인트</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr v-bind:key="i" v-for="(userVo, i) in reservationList">
-                        <td>{{ userVo.m_r_no }}</td>
-                        <td>{{ userVo.m_name }}</td>
-                        <td>{{ userVo.m_r_date }}</td>
-                        <td>{{ userVo.s_date }}</td>
-                        <td>1관</td>
-                        <td>{{ userVo.m_s_no }}</td>
-                        <td>{{ userVo.m_price }}</td>
-                        <td>{{ userVo.user_name }}</td>
-                        <td>{{ userVo.user_point }}</td>
-                      </tr>
-                    </tbody>
-                  </table>
+        <div id="wrap">
+            <AppHeader />
+            <div id="app">
+                <div class="content" v-if="isModalViewed">
+
+                    <!-- 모달 창 -->
+                    <ModalView :is-open="isModalViewed" @close-modal="isModalViewed = false"
+                        :selected-movie="selectedMovie">
+                        <FindContentView></FindContentView>
+                    </ModalView>
+
+                    <!-- 예매 내역 조회 창 -->
+                    <form action="" method="" enctype="multipart/form-data">
+                        <div class="m-body">
+                            <div id="result">
+                                <table id="result-table">
+                                    <thead>
+                                        <tr>
+                                            <th>예약번호</th>
+                                            <th>영화 제목</th>
+                                            <th>상영일자</th>
+                                            <th>상영시간</th>
+                                            <th>상영관</th>
+                                            <th>좌석번호</th>
+                                            <th>영화가격</th>
+                                            <th>구매자이름</th>
+                                            <th>포인트</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-bind:key="i" v-for="(userVo, i) in reservationList">
+
+                                            <td @click="openModal(userVo)">{{ userVo.m_r_no }}</td>
+                                            <td @click="openModal(userVo)">{{ userVo.m_name }}</td>
+                                            <td @click="openModal(userVo)">{{ userVo.m_r_date }}</td>
+                                            <td @click="openModal(userVo)">{{ userVo.s_date }}</td>
+                                            <td @click="openModal(userVo)">1관</td>
+                                            <td @click="openModal(userVo)">{{ userVo.m_s_no }}</td>
+                                            <td @click="openModal(userVo)">{{ userVo.m_price }}</td>
+                                            <td @click="openModal(userVo)">{{ userVo.user_name }}</td>
+                                            <td @click="openModal(userVo)">{{ userVo.user_point }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-              </div>
-            </form>
-          </div>
-          <div id="input-container">
-            <input type="text" v-model="phoneNumber" placeholder="010-0000-0000">
-            <button id="search-button" @click="goToFind">핸드폰번호조회</button>
-          </div>
-          <div class="dial-container">
-            <div class="dial-button" @click="appendNumber(1)">1</div>
-            <div class="dial-button" @click="appendNumber(2)">2</div>
-            <div class="dial-button" @click="appendNumber(3)">3</div>
-            <br>
-            <div class="dial-button" @click="appendNumber(4)">4</div>
-            <div class="dial-button" @click="appendNumber(5)">5</div>
-            <div class="dial-button" @click="appendNumber(6)">6</div>
-            <br>
-            <div class="dial-button" @click="appendNumber(7)">7</div>
-            <div class="dial-button" @click="appendNumber(8)">8</div>
-            <div class="dial-button" @click="appendNumber(9)">9</div>
-            <br>
-            <div class="dial-button" @click="appendNumber(0)">0</div>
-            <div class="dial-button" @click="deleteLastDigit">지움</div>
-            <div class="dial-button" @click="clearPhoneNumberMethod">전체삭제</div>
-          </div>
-          <div>
-            <router-link to="/" id="return-button">돌아가기</router-link>
-          </div>
+                <div id="input-container">
+                    <input type="text" v-model="phoneNumber" placeholder="010-0000-0000">
+                    <button id="search-button" @click="openReservationList">핸드폰번호조회</button>
+
+                </div>
+                <div class="dial-container">
+                    <div class="dial-button" @click="appendNumber(1)">1</div>
+                    <div class="dial-button" @click="appendNumber(2)">2</div>
+                    <div class="dial-button" @click="appendNumber(3)">3</div>
+                    <br>
+                    <div class="dial-button" @click="appendNumber(4)">4</div>
+                    <div class="dial-button" @click="appendNumber(5)">5</div>
+                    <div class="dial-button" @click="appendNumber(6)">6</div>
+                    <br>
+                    <div class="dial-button" @click="appendNumber(7)">7</div>
+                    <div class="dial-button" @click="appendNumber(8)">8</div>
+                    <div class="dial-button" @click="appendNumber(9)">9</div>
+                    <br>
+                    <div class="dial-button" @click="appendNumber(0)">0</div>
+                    <div class="dial-button" @click="deleteLastDigit">지움</div>
+                    <div class="dial-button" @click="clearPhoneNumberMethod">전체삭제</div>
+                </div>
+                <div>
+                    <router-link to="/" id="return-button">돌아가기</router-link>
+                </div>
+            </div>
+            <AppFooter />
         </div>
-        <AppFooter />
-      </div>
+
     </div>
-  </template>
-  
-  <script>
-  import "@/assets/css/FindView.css";
-  import AppHeader from "@/components/AppHeader.vue";
-  import AppFooter from "@/components/AppFooter.vue";
-  import FindContentView from "@/components/FindContentView.vue"; 
-  import axios from 'axios';
-  
-  export default {
+
+
+</template>
+
+<script>
+import "@/assets/css/FindView.css";
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+import FindContentView from "@/components/FindContentView.vue";
+import axios from 'axios';
+
+export default {
     name: "FindView",
     components: {
-      AppHeader,
-      AppFooter,
-      FindContentView
+        AppHeader,
+        AppFooter,
+        FindContentView,
     },
     data() {
-      return {
-        isModalViewed: false,
-        phoneNumber: '',
-        dateInput: '',
-        reservationList: [] 
-      };
+        return {
+            phoneNumber: '',
+            reservationList: [],
+            isModalViewed: false,
+            selectedMovie: null,
+        };
     },
     methods: {
-      getList() {
-        axios.get('http://localhost:9000/api/movie', {
-          params: {
-            phoneNumber: this.phoneNumber
-          }
-        })
-        .then(response => {
-          console.log(response.data);
-          this.reservationList = response.data.apiData;
-        })
-        .catch(error => {
-          console.log(error);
-        });
-      },
-      appendNumber(num) {
-        if (this.phoneNumber.length < 11) {
-          this.phoneNumber += num;
-        } else if (this.dateInput.length < 8) {
-          this.dateInput += num;
-        }
-      },
-      deleteLastDigit() {
-        if (this.dateInput.length > 0) {
-          this.dateInput = this.dateInput.slice(0, -1);
-        } else if (this.phoneNumber.length > 0) {
-          this.phoneNumber = this.phoneNumber.slice(0, -1);
-        }
-      },
-      clearPhoneNumberMethod() {
-        this.phoneNumber = '';
-      },
-      goToFind() {
-        this.isModalViewed = true;
-        this.$router.push({
-          name: 'FindContentView',
-          query: { phoneNumber: this.phoneNumber }
-        });
-      }
+
+        openModal(selectedMovie) {
+            this.selectedMovie = selectedMovie; // 선택된 영화 정보를 설정
+            this.isModalViewed = true; // 모달 창을 열기 위해 true로 설정
+        },
+
+        openReservationList() {
+            this.isModalViewed = true;
+            this.getList();
+        },
+        getList() {
+            axios({
+                method: 'get',
+                url: 'http://localhost:9000/api/movie',
+                params: { phoneNumber: this.phoneNumber },
+                responseType: 'json'
+            }).then(response => {
+                console.log(response);
+                console.log(response.data);
+                this.reservationList = response.data.apiData;
+            }).catch(error => {
+                console.log(error);
+            });
+        },
+        appendNumber(num) {
+            if (this.phoneNumber.length < 11) {
+                this.phoneNumber += num;
+            }
+        },
+        deleteLastDigit() {
+            if (this.phoneNumber.length > 0) {
+                this.phoneNumber = this.phoneNumber.slice(0, -1);
+            }
+        },
+        clearPhoneNumberMethod() {
+            this.phoneNumber = '';
+        },
     },
     created() {
-      // 컴포넌트가 생성될 때 데이터를 가져오도록 설정
+        // 컴포넌트가 생성될 때 데이터를 가져오도록 설정
+        this.getList(); // 핸드폰 번호 조회를 버튼 클릭 시에만 수행되도록 제거
     }
-  };
-  </script>
-  
+};
+</script>
